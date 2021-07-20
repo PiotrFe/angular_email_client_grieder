f71e83146732b4ab2410109004fc2d49356cd811
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2021-07-20 22:43:52 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'emailclient';
+  signedin$: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
+  }
+
+  noOnInit() {
+    this.authService.checkAuth().subscribe(() => {});
+    this.authService.signout().subscribe(() => {});
+  }
 }
